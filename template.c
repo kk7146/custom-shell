b7cdaf8b9cf5f95f070226d3009a94f840cf8c1b
kk7146 <kk21322132@icloud.com>
2024-10-15 19:55:54 +0900

    feat:ln_rm_ls

--- template.c
+++ template.c
@@ -1,9 +1,3 @@
-/* 
- * 4주차과제 
- * 2020136068
- * 손동은
- */
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -11,6 +5,9 @@
 #include <sys/stat.h>
 #include <sys/types.h>
 #include <dirent.h>
+#include <pwd.h>
+#include <grp.h>
+#include <time.h>
 
 #define MAX_CMD_SIZE    (128)
 #define BASE_DIR "/private/tmp/test"
@@ -42,44 +39,60 @@ void show_help() {
     printf("mkdir <path>       : Create a directory\n");
     printf("rmdir <path>       : Remove a directory\n");
     printf("rename <source> <target> : Rename a directory\n");
+    printf("ln <original> <new>: Create a hard link\n");
+    printf("rm <file>          : Remove a file\n");
     printf("ls                 : List current directory contents\n");
     printf("quit               : Exit the shell\n");
 }
 
-void ls_func()
-{
+void print_permissions(mode_t mode) {
+    printf((S_ISDIR(mode)) ? "d" : "-");
+    printf((mode & S_IRUSR) ? "r" : "-");
+    printf((mode & S_IWUSR) ? "w" : "-");
+    printf((mode & S_IXUSR) ? "x" : "-");
+    printf((mode & S_IRGRP) ? "r" : "-");
+    printf((mode & S_IWGRP) ? "w" : "-");
+    printf((mode & S_IXGRP) ? "x" : "-");
+    printf((mode & S_IROTH) ? "r" : "-");
+    printf((mode & S_IWOTH) ? "w" : "-");
+    printf((mode & S_IXOTH) ? "x" : "-");
+}
+
+void ls_func() {
     DIR *dir;
     struct dirent *entry;
     struct stat statbuf;
-
+    struct passwd *pw;
+    struct group *gr;
+    char timebuf[64];
     dir = opendir(".");
     if (dir == NULL) {
         perror("ls");
         return;
     }
-
     while ((entry = readdir(dir)) != NULL) {
         if (stat(entry->d_name, &statbuf) == -1) {
             perror("stat");
             continue;
         }
-        if (S_ISDIR(statbuf.st_mode)) {
-            printf("[DIR] %s\n", entry->d_name);
-        } else if (S_ISREG(statbuf.st_mode)) {
-            printf("[FILE] %s\n", entry->d_name);
-        } else {
-            printf("[OTHER] %s\n", entry->d_name);
-        }
+        print_permissions(statbuf.st_mode);
+        printf(" %hd ", statbuf.st_nlink);
+        pw = getpwuid(statbuf.st_uid);
+        gr = getgrgid(statbuf.st_gid);
+        printf("%s %s ", pw->pw_name, gr->gr_name);
+        printf("%5lld ", statbuf.st_size);
+        strftime(timebuf, sizeof(timebuf), "%b %d %H:%M", localtime(&statbuf.st_atime));
+        printf("Access: %s ", timebuf);
+        strftime(timebuf, sizeof(timebuf), "%b %d %H:%M", localtime(&statbuf.st_mtime));
+        printf("Modify: %s ", timebuf);
+        printf("%s\n", entry->d_name);
     }
-
     closedir(dir);
 }
 
-int main(int argc, char **argv)
-{
+int main(int argc, char **argv) {
     char *command, *tok_str;
     char *current_dir;
-
     struct stat st;
     if (stat(BASE_DIR, &st) != 0) {
         if (mkdir(BASE_DIR, 0755) != 0) {
@@ -87,21 +100,17 @@ int main(int argc, char **argv)
             return 1;
         }
     }
-
     if (!validate_path(current_dir)) {
         chdir(BASE_DIR);
     }
-
     command = (char*) malloc(MAX_CMD_SIZE);
     if (command == NULL) {
         perror("malloc");
         exit(1);
     }
-
     do {
         current_dir = getcwd(NULL, 0);
-        if (!validate_path(current_dir))
-        {
+        if (!validate_path(current_dir)) {
             chdir(BASE_DIR);
             current_dir = getcwd(NULL, 0);
         }
@@ -117,62 +126,66 @@ int main(int argc, char **argv)
             tok_str = resolve_path(tok_str);
             if (tok_str == NULL) {
                 printf("cd: missing argument\n");
-            }
-            else if (!validate_path(current_dir))
-            {
+            } else if (!validate_path(current_dir)) {
                 perror("cd");
-            }
-            else if (chdir(tok_str) != 0)
+            } else if (chdir(tok_str) != 0)
                 perror("cd");
-        }
-        else if (strcmp(tok_str, "help") == 0)
+        } else if (strcmp(tok_str, "help") == 0)
             show_help();
         else if (strcmp(tok_str, "mkdir") == 0) {
             tok_str = strtok(NULL, " \n");
             tok_str = resolve_path(tok_str);
             if (tok_str == NULL) {
                 printf("mkdir: missing argument\n");
-            }
-            else if (!validate_path(tok_str)) {
+            } else if (!validate_path(tok_str)) {
                 printf("mkdir: cannot create directory\n");
-            }
-            else {
+            } else {
                 mkdir(tok_str, 0777);
             }
-        }
-        else if (strcmp(tok_str, "rmdir") == 0) {
+        } else if (strcmp(tok_str, "rmdir") == 0) {
             tok_str = strtok(NULL, " \n");
             tok_str = resolve_path(tok_str);
             if (tok_str == NULL) {
                 printf("rmdir: missing argument\n");
-            }
-            else if (!validate_path(tok_str)) {
+            } else if (!validate_path(tok_str)) {
                 printf("rmdir: failed to remove\n");
-            }
-            else {
+            } else {
                 rmdir(tok_str);
             }
-        }
-        else if (strcmp(tok_str, "rename") == 0) {
+        } else if (strcmp(tok_str, "rename") == 0) {
             char *src = strtok(NULL, " \n");
             char *dest = strtok(NULL, " \n");
             if (src == NULL || dest == NULL) {
                 printf("rename: missing source or target argument\n");
-            } 
-            else if (!validate_path(resolve_path(src)) && !validate_path(resolve_path(dest))) {
+            } else if (!validate_path(resolve_path(src)) && !validate_path(resolve_path(dest))) {
                 printf("rename: failed to rename\n");
-            }
-            else {
+            } else {
                 rename(src, dest);
             }
-        } 
-        else if (strcmp(tok_str, "ls") == 0)
+        } else if (strcmp(tok_str, "ln") == 0) {
+            char *original = strtok(NULL, " \n");
+            char *new_link = strtok(NULL, " \n");
+            if (original == NULL || new_link == NULL) {
+                printf("ln: missing original or new link argument\n");
+            } else if (link(original, new_link) != 0) {
+                perror("ln");
+            } else {
+                printf("Hard link created: %s -> %s\n", new_link, original);
+            }
+        } else if (strcmp(tok_str, "rm") == 0) {
+            char *file = strtok(NULL, " \n");
+            if (file == NULL) {
+                printf("rm: missing argument\n");
+            } else if (unlink(file) != 0) {
+                perror("rm");
+            } else {
+                printf("File removed: %s\n", file);
+            }
+        } else if (strcmp(tok_str, "ls") == 0)
             ls_func();
         else {
-            // TODO: implement functions
             printf("your command: %s\n", tok_str);
             printf("and argument is ");
-
             tok_str = strtok(NULL, " \n");
             if (tok_str == NULL) {
                 printf("NULL\n");
